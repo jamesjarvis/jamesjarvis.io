e56c3e872726b838bbdcda350a99809f0d57031c
James Jarvis <jjhaslanded@gmail.com>
2019-01-25 19:48:42 -0800

    Updates

---
+++ precache-manifest.3354fcb847df5c4bdef550adceceffaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3da1cfd2eaea0c091a2",
+    "url": "/static/css/main.46d05656.chunk.css"
+  },
+  {
+    "revision": "f3da1cfd2eaea0c091a2",
+    "url": "/static/js/main.f3da1cfd.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83603b5b3272271369374f1d5c264d4e",
+    "url": "/index.html"
+  }
+];
