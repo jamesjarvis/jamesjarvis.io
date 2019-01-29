c63d1bf3f60404eec890d723e1a46e77cf2a5728
James Jarvis <jjhaslanded@gmail.com>
2019-01-29 09:13:29 -0800

    added a bit of SEO

--- src/components/wrapper.jsx
+++ src/components/wrapper.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 import Header from './common/header';
 import Footer from './common/footer';
 
@@ -7,6 +8,11 @@ export default class Wrapper extends React.Component {
   render() {
     return (
       <>
+        <Helmet>
+          <title>James Jarvis</title>
+          <meta name="description" content="James Jarvis' personal site" />
+          <meta name="keywords" cpntent="personal,site,portfolio,developer" />
+        </Helmet>
         <Header />
         {this.props.children}
         <Footer />
