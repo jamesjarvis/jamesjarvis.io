678b991beabc3bde1976d7b839e81e9ff9155d43
James Jarvis <jjhaslanded@gmail.com>
2019-02-03 21:24:13 -0800

    forgot to commit this file

---
+++ src/templates/blogPost.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Wrapper from '../components/containers/wrapper';
+import ComplexWrapper from '../components/containers/complexWrapper';
+import Tech from '../components/tech/tech';
+
+const Template = ({ data }) => {
+  const { post } = data; // data.markdownRemark holds our post data
+  return (
+    <Wrapper title={post.frontmatter.title}>
+      <ComplexWrapper>
+        <h1>{post.frontmatter.title}</h1>
+        <span className={'date'}>
+          {post.frontmatter.date}
+          {/* <Tech techs={post.frontmatter.tech} /> */}
+        </span>
+        <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: post.html }} />
+      </ComplexWrapper>
+    </Wrapper>
+  );
+};
+
+Template.propTypes = {
+  data: PropTypes.any.isRequired,
+};
+
+export default Template;
+
+export const pageQuery = graphql`
+  query BlogPostQuery($slug: String!) {
+    post: markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM, YYYY")
+        tech
+      }
+    }
+  }
+`;
