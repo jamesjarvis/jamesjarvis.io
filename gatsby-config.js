2abc5a901c70ef84db60fe849964ae0173003b08
James Jarvis <jjhaslanded@gmail.com>
2019-02-23 17:41:25 -0800

    Fixes #3 - updates manifest generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,9 +46,18 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-favicon`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        logo: './assets/images/avatar.jpg',
+        name: `James Jarvis`,
+        short_name: `James Jarvis`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#004196`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `assets/images/avatar.jpg`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
       },
     },
     {
