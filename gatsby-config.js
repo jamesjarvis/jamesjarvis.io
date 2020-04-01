8b27fbd07aa92b80a1a2266d6cc054c9da16ca86
James Jarvis <jarjames@cisco.com>
2020-04-01 14:01:01 +0100

    :bug: fix wrong environment passing

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "deve
 console.log(`Using environment config: '${activeEnv}'`);
 
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${activeEnv}`,
 });
 
 module.exports = {
